f7f95fd911095568b0ac1e6634c5f793c26e34fa
facunare <arechaga.facundoet36@gmail.com>
2024-02-27 11:12:47 -0300

    a

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(morgan("tiny"))
 app.use(cors(
   {
-      origin: ["https://facundoarechaga.vercel.app/"],
+      origin: ["https://facundoarechaga.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
   }
